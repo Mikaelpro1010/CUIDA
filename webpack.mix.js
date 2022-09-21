2a534872ce8bc777e8af802ee7b42fc8d06c1576
SauloDc <sdc2decastro@gmail.com>
2022-09-21 17:32:19 -0300

    INstalando bootstrap via npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +10,10 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+  .sass("resources/scss/style.scss", "public/css/style.css")
+  .scripts("node_modules/jquery/dist/jquery.js", "public/js/jquery.js")
+  .scripts(
+    "node_modules/bootstrap/dist/js/bootstrap.bundle.js",
+    "public/js/bootstrap.js"
+  );
