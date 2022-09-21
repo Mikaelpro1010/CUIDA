b314449c38968c8cb4973facce932588f2ef156d
SauloDc <sdc2decastro@gmail.com>
2022-09-21 17:29:00 -0300

    Alterações no template

--- resources/views/template/simple_page.blade.php
+++ resources/views/template/simple_page.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 
@@ -13,9 +13,8 @@
 
     <title>@yield('titulo', 'EscutaSol')</title>
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/style.css') }}">
     <script src="{{ asset('js/fontawesome.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
 </head>
 
 <body class="bg-light">
@@ -35,12 +34,18 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav me-auto pb-2 pb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('politicas') }}">Políticas de Privacidade</a>
+                            <a class="nav-link text-white @if(Route::is('politicas')) border-bottom border-3 border-info @endif"
+                                href="{{ route('politicas') }}">
+                                Políticas de Privacidade
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('termos') }}">Termos de Uso</a>
+                            <a class="nav-link text-white @if(Route::is('termos')) border-bottom border-3 border-info @endif"
+                                href="{{ route('termos') }}">
+                                Termos de Uso
+                            </a>
                         </li>
                     </ul>
 
@@ -58,7 +63,7 @@
 
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a class="dropdown-item" href="{{ route('inicio') }}">inicio</a>
+                                    <a class="dropdown-item" href="{{ route('inicio') }}">Inicio</a>
                                     <a class="dropdown-item" href="{{ route('inicio') }}">Manifestações</a>
                                     <a class="dropdown-item" href="{{ route('mensagens') }}">Mensagens</a>
                                     <hr>
@@ -90,24 +95,45 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link @if(routeClass()::currentRouteName() == 'inicio') border-bottom border-3 border-primary @endif"
+                        <a class="nav-link @if(Route::is('inicio')) border-bottom border-3 border-primary @endif"
                             href="{{ route('inicio') }}">
                             Inicio
                         </a>
-                        <a class="nav-link @if(routeClass()::currentRouteName() == 'manifestacoes') border-bottom border-3 border-primary @endif"
+                        <a class="nav-link @if(Route::is('manifestacoes')) border-bottom border-3 border-primary @endif"
                             href="{{ route('manifestacoes') }}">
                             Manifestações
                             <span class="badge rounded-pill bg-danger">
                                 {{ manifestacoesNaoEncerradasNotification() }}
                             </span>
                         </a>
-                        <a class="nav-link @if(routeClass()::currentRouteName() == 'mensagens') border-bottom border-3 border-primary @endif"
+                        <a class="nav-link @if(Route::is('mensagens')) border-bottom border-3 border-primary @endif"
                             href="{{ route('mensagens') }}">
                             Mensagens
                             <span class="badge rounded-pill bg-danger">
                                 {{ canaisAguardandoRespostaNotification() }}
                             </span>
                         </a>
+                        <li class="nav-item dropdown @if(Route::current()->action['namespace'] == 
+                            'App\Http\Controllers\Avaliacoes') border-bottom border-3 border-primary @endif">
+                            <a class=" nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Avaliações
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="{{ route('resumo-avaliacoes') }}">Resumos</a></li>
+                                <li><a class="dropdown-item" href="{{ route('unidades-secr-list') }}">Unidades</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Configurações
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Secretarias</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                            </ul>
+                        </li>
                     </div>
                 </div>
             </div>
@@ -128,21 +154,23 @@
         </div>
         @endif
         @if ($errors->any())
-        @foreach ($errors->all() as $error)
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
+            @endforeach
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-        @endforeach
         @endif
         @yield('main')
     </div>
 
     <footer class="d-flex justify-content-center mt-2 mb-3 mx-3">
-        <div class="border-bottom text-center">EscutaSol - Controladoria e Ouvidoria Geral do Municipio de
-            Sobral - CGM - 2022</div>
+        <div class="border-bottom text-center">
+            EscutaSol - Controladoria e Ouvidoria Geral do Municipio de Sobral - CGM - 2022
+        </div>
     </footer>
-    <script src="{{ asset('js/jquery-3.6.0.js') }}"></script>
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
     <script src="{{ asset('js/sweetalert2.all.min.js') }}"></script>
     @yield('scripts')
     <script>
