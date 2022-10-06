8244afb18b274874691b6464243a39bba9c8e7fb
SauloDc <sdc2decastro@gmail.com>
2022-10-06 08:34:55 -0300

    atualizacoes no webpack.mix inserindo charts.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ let mix = require("laravel-mix");
 mix
   .sass("resources/scss/style.scss", "public/css/style.css")
   .scripts("node_modules/jquery/dist/jquery.js", "public/js/jquery.js")
+  .scripts("node_modules/chart.js/dist/chart.js", "public/js/chart.js")
   .scripts(
     "node_modules/bootstrap/dist/js/bootstrap.bundle.js",
     "public/js/bootstrap.js"
