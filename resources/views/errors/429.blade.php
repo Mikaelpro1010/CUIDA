dd2138ff23393e60364b11810aacf43c78019e99
SauloDc <sdc2decastro@gmail.com>
2022-10-17 15:14:15 -0300

    Novas telas de erro para Too many requests e para Manutenção

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,11 @@
+@extends('template.base')
+
+@section('titulo', 'Ultrapassou o Limite')
+
+@section('content')
+<div class="text-center">
+    <h1>Limite Ultrapassado!</h1>
+    <h5>Você ultrapassou o Limite de Avaliações! Volte Amanhã caso queira fazer mais avaliações</h5>
+    <a class="btn btn-primary mt-3" href="{{ route('home') }}">Início</a>
+</div>
+@endsection
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,10 @@
+@extends('template.base')
+
+@section('titulo', 'Em Manutenção')
+
+@section('content')
+<div class="text-center">
+    <h1>Estamos em Manutenção!</h1>
+    <h3>Voltaremos em alguns instantes!</h3>
+</div>
+@endsection
