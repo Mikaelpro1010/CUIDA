20c9fba99ff078e2773d4f4f05a877795f4b8839
SauloDc <sdc2decastro@gmail.com>
2022-10-20 11:11:56 -0300

    Alteraçoes no texto do erro

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="text-center">
     <h1>Limite Ultrapassado!</h1>
-    <h5>Você ultrapassou o Limite de Avaliações! Volte Amanhã caso queira fazer mais avaliações</h5>
+    <h5>Você ultrapassou o Limite diário! Volte Amanhã caso queira fazer mais avaliações</h5>
     <a class="btn btn-primary mt-3" href="{{ route('home') }}">Início</a>
 </div>
 @endsection
