21bc0b67e98fcf4709b8756c095b71b6949226ff
SauloDc <sdc2decastro@gmail.com>
2022-10-20 10:58:14 -0300

    Inserindo novos metodos nos modelos para utilizar na pagina de resumo das avaliacoes

--- app/Models/Avaliacao/Unidade.php
+++ app/Models/Avaliacao/Unidade.php
@@ -6,6 +6,7 @@ use App\Models\Secretaria;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Cache;
 
 /**
  * Unidade de uma Secretaria
@@ -23,5 +24,77 @@ class Unidade extends Model
     public function avaliacoes(): HasMany
     {
         return $this->hasMany(Avaliacao::class, 'unidade_secr_id', 'id');
+    }
+
+    public static function getResumoUnidadeId($id): array
+    {
+        $unidade = Unidade::find($id);
+        return $unidade->getResumoFromCache();
+    }
+
+    public function getResumoFromCache(): array
+    {
+        return
+            Cache::rememberForever('Unidade_' . $this->id, function () {
+                $this->update([
+                    'nota' => $this->avaliacoes->avg('nota')
+                ]);
+
+                $notas1 = $this->avaliacoes->where('nota', 1)->count();
+                $notas2 = $this->avaliacoes->where('nota', 2)->count();
+                $notas3 = $this->avaliacoes->where('nota', 3)->count();
+                $notas4 = $this->avaliacoes->where('nota', 4)->count();
+                $notas5 = $this->avaliacoes->where('nota', 5)->count();
+
+                $qtdAvaliacoes = $notas1 + $notas2 + $notas3 + $notas4 + $notas5;
+
+                return [
+                    'qtd' => $qtdAvaliacoes,
+                    'notas1' => $notas1,
+                    'notas2' => $notas2,
+                    'notas3' => $notas3,
+                    'notas4' => $notas4,
+                    'notas5' => $notas5,
+                ];
+            });
+    }
+
+    public function updateResumoCache(Avaliacao $avaliacao): void
+    {
+        if (!is_null(Cache::get('Unidade_' . $avaliacao->unidade_secr_id))) {
+            $unidadeInfo =  Cache::get('Unidade_' . $avaliacao->unidade_secr_id);
+
+            switch ($avaliacao->nota) {
+                case 1:
+                    $unidadeInfo['notas1']++;
+                    break;
+                case 2:
+                    $unidadeInfo['notas2']++;
+                    break;
+                case 3:
+                    $unidadeInfo['notas3']++;
+                    break;
+                case 4:
+                    $unidadeInfo['notas4']++;
+                    break;
+                case 5:
+                    $unidadeInfo['notas5']++;
+                    break;
+            }
+
+            $media = ($unidadeInfo['notas1'] + ($unidadeInfo['notas2'] * 2) + ($unidadeInfo['notas3'] * 3) + ($unidadeInfo['notas4'] * 4) + ($unidadeInfo['notas5'] * 5))
+                / ($unidadeInfo['notas1'] + $unidadeInfo['notas2'] + $unidadeInfo['notas3'] + $unidadeInfo['notas4'] + $unidadeInfo['notas5']);
+
+            $unidadeInfo['qtd']++;
+
+            $this->update([
+                'nota' =>  $media
+            ]);
+
+            Cache::forget('Unidade_' . $avaliacao->unidade_secr_id);
+            Cache::forever('Unidade_' . $avaliacao->unidade_secr_id, $unidadeInfo);
+        } else {
+            $this->getResumoFromCache();
+        }
     }
 }
--- app/Models/Secretaria.php
+++ app/Models/Secretaria.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Support\Facades\Cache;
 
 class Secretaria extends Model
 {
@@ -29,4 +30,63 @@ class Secretaria extends Model
         return $this->hasManyThrough(Avaliacao::class, Unidade::class, 'secretaria_id', 'unidade_secr_id');
     }
 
+    public static function getResumoSecretariaId($id): array
+    {
+        $secretaria = Secretaria::find($id);
+        return $secretaria->getResumo();
+    }
+
+    public static function getResumoSecretariaAll(): array
+    {
+        $secretarias = Secretaria::query()->with('unidades')->get();
+
+        $resumoSecretarias = [
+            'qtd' => 0,
+            'notas1' => 0,
+            'notas2' => 0,
+            'notas3' => 0,
+            'notas4' => 0,
+            'notas5' => 0,
+        ];
+
+        foreach ($secretarias as $secretaria) {
+            $resumoSecretaria = $secretaria->getResumo();
+            $resumoSecretarias['qtd'] += $resumoSecretaria['qtd'];
+            $resumoSecretarias['notas1'] += $resumoSecretaria['notas1'];
+            $resumoSecretarias['notas2'] += $resumoSecretaria['notas2'];
+            $resumoSecretarias['notas3'] += $resumoSecretaria['notas3'];
+            $resumoSecretarias['notas4'] += $resumoSecretaria['notas4'];
+            $resumoSecretarias['notas5'] += $resumoSecretaria['notas5'];
+        }
+
+        return $resumoSecretarias;
+    }
+
+    public function getResumo(): array
+    {
+        $resumoSecretaria = [
+            'qtd' => 0,
+            'notas1' => 0,
+            'notas2' => 0,
+            'notas3' => 0,
+            'notas4' => 0,
+            'notas5' => 0,
+        ];
+
+        foreach ($this->unidades as $unidade) {
+            $resumoUnidade = $unidade->getResumoFromCache();
+            $resumoSecretaria['qtd'] += $resumoUnidade['qtd'];
+            $resumoSecretaria['notas1'] += $resumoUnidade['notas1'];
+            $resumoSecretaria['notas2'] += $resumoUnidade['notas2'];
+            $resumoSecretaria['notas3'] += $resumoUnidade['notas3'];
+            $resumoSecretaria['notas4'] += $resumoUnidade['notas4'];
+            $resumoSecretaria['notas5'] += $resumoUnidade['notas5'];
+        }
+
+        $this->update([
+            'nota' => $this->unidades->avg('nota')
+        ]);
+
+        return $resumoSecretaria;
+    }
 }
