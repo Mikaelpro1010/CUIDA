cc4e51deebbd6058ea8d566efd27217580ed2525
SauloDc <sdc2decastro@gmail.com>
2022-10-21 17:02:26 -0300

    Controller para os tipos d usuarios

---
+++ app/Http/Controllers/Configs/RolesController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers\Configs;
+
+use App\Constants\Permission;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Permission as ModelsPermission;
+use App\Models\Role;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
+
+class RolesController extends Controller
+{
+    public function listRoles()
+    {
+        // $this->authorize(Permission::GERENCIAR_USUARIOS_LIST);
+
+        $roles = Role::query()
+            ->when(
+                request()->pesquisa,
+                function ($query) {
+                    return $query
+                        ->where(
+                            'name',
+                            'like',
+                            '%' . request()->pesquisa . '%'
+                        );
+                }
+            )
+            ->with('users')
+            ->orderBy('updated_at', 'desc')
+            ->paginate(15)
+            ->appends([
+                'pesquisa' => request()->pesquisa,
+                'tipo_usuario' => request()->tipo_usuario,
+            ]);
+
+        return view('admin.config.tipos-usuario.list-user-types', compact('roles'));
+    }
+
+    public function viewRole(Role $role)
+    {
+        // $this->authorize(Permission::GERENCIAR_USUARIOS_VIEW);
+
+        $permissionGroups = Permission::PERMISSIONS;
+
+        return view('admin.config.tipos-usuario.see-user-type', compact('role', 'permissionGroups'));
+    }
+
+    public function createRole(Request $request)
+    {
+        // $this->authorize(Permission::GERENCIAR_USUARIOS_CREATE);
+        $permissionGroups = Permission::PERMISSIONS;
+
+        return view('admin.config.tipos-usuario.create-user-type', compact('permissionGroups'));
+    }
+
+    public function storeRole(Request $request)
+    {
+        // $this->authorize(Permission::GERENCIAR_USUARIOS_CREATE);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string|max:255',
+            ],
+            [
+                'required' => 'O campo :atribute não pode ser vazio!',
+                'name.max' => 'O campo Nome não pode conter mais de 255 caracteres!',
+            ],
+            [
+                'name' => 'Nome',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $role = Role::create([
+            'name' => $request->name
+        ]);
+
+        $role->permissions()->toggle($request->permissions);
+
+        return redirect()->route('get-roles-list')->with(['success' => 'Tipo de Usuário Cadastrado com Sucesso!']);
+    }
+
+    public function editRole(Role $role)
+    {
+        // $this->authorize(Permission::GERENCIAR_USUARIOS_EDIT);
+        $permissionGroups = Permission::PERMISSIONS;
+
+        return view('admin.config.tipos-usuario.edit-user-type', compact('role', 'permissionGroups'));
+    }
+
+    public function updateRole(Role $role, Request $request)
+    {
+        // $this->authorize(Permission::GERENCIAR_USUARIOS_EDIT);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string|max:255',
+            ],
+            [
+                'name.required' => 'O campo Nome não pode ser vazio!',
+                'name.max' => 'O campo Nome não pode conter mais de 255 caracteres!',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $role->update([
+            'name' => $request->name,
+        ]);
+
+        $role->permissions()->sync($request->permissions);
+        Cache::forget('role-' . $role->id . '-permissions');
+
+        return redirect()->route('get-roles-list')->with(['success' => 'Tipos de Usuário editado com Sucesso!']);
+    }
+
+    public function deleteRole(Role $role)
+    {
+        // $this->authorize(Permission::GERENCIAR_USUARIOS_DELETE);
+
+        if ($role->users->isNotEmpty()) {
+            return redirect()->route('get-roles-list')->withErrors(['Erro' => 'Não é possivel deletar um tipo de Usuario que contenha Usuários ativos!']);
+        } else {
+            $role->delete();
+            return redirect()->route('get-roles-list')->with(['success' => 'Tipo de Usuário deletado com Sucesso!']);
+        }
+    }
+}
