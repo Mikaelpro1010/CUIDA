3db8d7a767c5b3c53064a3d34ccbb77bdc1f2e2d
SauloDc <sdc2decastro@gmail.com>
2022-10-26 17:00:48 -0300

    alterando o calculo da avaliacao da nota

--- resources/views/admin/avaliacoes/resumos/components/avaliacao-geral.blade.php
+++ resources/views/admin/avaliacoes/resumos/components/avaliacao-geral.blade.php
@@ -7,54 +7,53 @@
             <div class=" p-3">
                 <div class="d-flex justify-content-between align-items-end">
                     <div class="text-center text-danger">
-                        <i
-                            class="@if ($avaliacoesAverage < 1.5) fa-6x @else fa-4x @endif  fa-regular fa-face-angry"></i>
+                        <i class="@if ($avaliacoesAverage < 1) fa-6x @else fa-4x @endif  fa-regular fa-face-angry"></i>
                         <br>
                         <span>Muito Ruim</span>
                     </div>
                     <div class="text-center text-warning">
                         <i
-                            class="@if ($avaliacoesAverage >= 1.5 && $avaliacoesAverage < 2.5) fa-6x @else fa-4x @endif fa-regular fa-face-frown"></i>
+                            class="@if ($avaliacoesAverage >= 1 && $avaliacoesAverage < 2) fa-6x @else fa-4x @endif fa-regular fa-face-frown"></i>
                         <br>
                         <span>Ruim</span>
                     </div>
                     <div class="text-center text-info">
                         <i
-                            class="@if ($avaliacoesAverage >= 2.5 && $avaliacoesAverage < 3.5) fa-6x @else fa-4x @endif  fa-regular fa-face-meh"></i>
+                            class="@if ($avaliacoesAverage >= 2 && $avaliacoesAverage < 3) fa-6x @else fa-4x @endif  fa-regular fa-face-meh"></i>
                         <br>
                         <span>Neutro</span>
                     </div>
 
                     <div class="text-center text-primary">
                         <i
-                            class="@if ($avaliacoesAverage >= 3.5 && $avaliacoesAverage < 4.5) fa-6x @else fa-4x @endif  fa-regular fa-face-smile"></i>
+                            class="@if ($avaliacoesAverage >= 3 && $avaliacoesAverage < 4) fa-6x @else fa-4x @endif  fa-regular fa-face-smile"></i>
                         <br>
                         <span>Bom</span>
                     </div>
                     <div class="text-center text-success">
                         <i
-                            class="@if ($avaliacoesAverage >= 4.5) fa-6x @else fa-4x @endif fa-regular fa-face-laugh-beam"></i>
+                            class="@if ($avaliacoesAverage >= 4) fa-6x @else fa-4x @endif fa-regular fa-face-laugh-beam"></i>
                         <br>
                         <span>Muito Bom</span>
                     </div>
                 </div>
                 <div class="mt-2 progress">
                     <div class="progress-bar progress-bar-striped progress-bar-animated
-                                    @if ($avaliacoesAverage < 1.5)
+                                    @if ($avaliacoesAverage < 1)
                                         bg-danger
                                     @else
-                                        @if ($avaliacoesAverage >=  1.5 && $avaliacoesAverage < 2.5)
-                                        bg-warning                            
+                                        @if ($avaliacoesAverage >=  1 && $avaliacoesAverage < 2)
+                                            bg-warning                            
                                         @else
-                                        @if ($avaliacoesAverage >=  2.5 && $avaliacoesAverage < 3.5)
-                                        bg-info                            
-                                        @else
-                                        @if ($avaliacoesAverage >=  3.5 && $avaliacoesAverage < 4.5)
-                                        bg-primary                            
-                                        @else
-                                        bg-success                            
-                                        @endif                
-                                        @endif    
+                                            @if ($avaliacoesAverage >=  2 && $avaliacoesAverage < 3)
+                                                bg-info                            
+                                            @else
+                                                @if ($avaliacoesAverage >=  3 && $avaliacoesAverage < 4)
+                                                    bg-primary                            
+                                                @else
+                                                    bg-success                            
+                                                @endif                
+                                            @endif    
                                         @endif    
                                     @endif
                                     " role="progressbar" aria-label="Animated striped example"
@@ -67,16 +66,16 @@
             <div class="text-center">
                 <span class="fs-3 fw-normal">Avaliação Geral:</span>
                 <p class="fs-1 fw-bold 
-                                    @if ($avaliacoesAverage < 1.5)
+                                    @if ($avaliacoesAverage < 1)
                                     text-danger
                                     @else
-                                        @if ($avaliacoesAverage >=  1.5 && $avaliacoesAverage < 2.5)
+                                        @if ($avaliacoesAverage >=  1 && $avaliacoesAverage < 2)
                                         text-warning                            
                                         @else
-                                            @if ($avaliacoesAverage >=  2.5 && $avaliacoesAverage < 3.5)
+                                            @if ($avaliacoesAverage >=  2 && $avaliacoesAverage < 3)
                                             text-info                            
                                             @else
-                                                @if ($avaliacoesAverage >=  3.5 && $avaliacoesAverage < 4.5)
+                                                @if ($avaliacoesAverage >=  3 && $avaliacoesAverage < 4)
                                                 text-primary                            
                                                 @else
                                                 text-success                            
