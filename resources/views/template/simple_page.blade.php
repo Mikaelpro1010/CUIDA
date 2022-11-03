b6220b2e219ed43cf6e1a0f23262dc135edea9f3
SauloDc <sdc2decastro@gmail.com>
2022-11-03 11:21:42 -0300

    Atualizano o layout

--- resources/views/template/simple_page.blade.php
+++ resources/views/template/simple_page.blade.php
@@ -50,151 +50,149 @@
                     <ul class="nav navbar-nav navbar-right text-white gap-2">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a class="btn btn-outline-light" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="btn btn-outline-light" href="{{ route('register') }}">Register</a></li>
+                        <li><a class="btn btn-outline-light" href="{{ route('login') }}">Login</a></li>
+                        <li><a class="btn btn-outline-light" href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle text-white" data-bs-toggle="dropdown"
-                                    role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ auth()->user()->name }} <span class="caret"></span>
-                                </a>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle text-white" data-bs-toggle="dropdown" role="button"
+                                aria-expanded="false" aria-haspopup="true" v-pre>
+                                {{ auth()->user()->name }} <span class="caret"></span>
+                            </a>
 
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a class="dropdown-item" href="{{ route('inicio') }}">Inicio</a>
-                                        <a class="dropdown-item" href="{{ route('inicio') }}">Manifestações</a>
-                                        <a class="dropdown-item" href="{{ route('mensagens') }}">Mensagens</a>
-                                        <hr>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('inicio') }}">Inicio</a>
+                                    <a class="dropdown-item" href="{{ route('inicio') }}">Manifestações</a>
+                                    <a class="dropdown-item" href="{{ route('mensagens') }}">Mensagens</a>
+                                    <hr>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                            style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
         @auth
-            <div class="navbar navbar-expand-lg bg-warning py-0">
-                <div class="container">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon "></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link @if (Route::is('inicio')) border-bottom border-3 border-primary @endif"
-                                href="{{ route('inicio') }}">
-                                Inicio
+        <div class="navbar navbar-expand-lg bg-warning py-0">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon "></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link @if (Route::is('inicio')) border-bottom border-3 border-primary @endif"
+                            href="{{ route('inicio') }}">
+                            Inicio
+                        </a>
+                        @can(permissionConstant()::MANIFESTACAO_LIST)
+                        <a class="nav-link @if (Route::is('manifestacoes')) border-bottom border-3 border-primary @endif"
+                            href="{{ route('manifestacoes') }}">
+                            Manifestações
+                            <span class="badge rounded-pill bg-danger">
+                                {{ manifestacoesNaoEncerradasNotification() }}
+                            </span>
+                        </a>
+                        @endcan
+                        @can(permissionConstant()::MANIFESTACAO_CHAT)
+                        <a class="nav-link @if (Route::is('mensagens')) border-bottom border-3 border-primary @endif"
+                            href="{{ route('mensagens') }}">
+                            Mensagens
+                            <span class="badge rounded-pill bg-danger">
+                                {{ canaisAguardandoRespostaNotification() }}
+                            </span>
+                        </a>
+                        @endcan
+                        <li
+                            class="nav-item dropdown @if (Route::current()->action['namespace'] == 'App\Http\Controllers\Avaliacoes') border-bottom border-3 border-primary @endif">
+                            <a class=" nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Avaliações
                             </a>
-                            @can(permissionConstant()::MANIFESTACAO_LIST)
-                                <a class="nav-link @if (Route::is('manifestacoes')) border-bottom border-3 border-primary @endif"
-                                    href="{{ route('manifestacoes') }}">
-                                    Manifestações
-                                    <span class="badge rounded-pill bg-danger">
-                                        {{ manifestacoesNaoEncerradasNotification() }}
-                                    </span>
-                                </a>
-                            @endcan
-                            @can(permissionConstant()::MANIFESTACAO_CHAT)
-                                <a class="nav-link @if (Route::is('mensagens')) border-bottom border-3 border-primary @endif"
-                                    href="{{ route('mensagens') }}">
-                                    Mensagens
-                                    <span class="badge rounded-pill bg-danger">
-                                        {{ canaisAguardandoRespostaNotification() }}
-                                    </span>
-                                </a>
-                            @endcan
-                            <li
-                                class="nav-item dropdown @if (Route::current()->action['namespace'] == 'App\Http\Controllers\Avaliacoes') border-bottom border-3 border-primary @endif">
-                                <a class=" nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Avaliações
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item"
-                                            href="
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="
                                     @if (auth()->user()->can(permissionConstant()::RELATORIO_AVALIACOES_GERAL_VIEW)) {{ route('resumo-avaliacoes') }}
                                     @else
                                     {{ route('resumo-avaliacoes-secretaria-list') }} @endif
                                     ">Resumos</a>
-                                    </li>
-                                    @can(permissionConstant()::UNIDADE_SECRETARIA_LIST)
-                                        <li><a class="dropdown-item" href="{{ route('unidades-secr-list') }}">Unidades</a></li>
-                                    @endcan
-                                </ul>
-                            </li>
-                            @if (auth()->user()->can(permissionConstant()::GERENCIAR_USUARIOS_LIST) ||
-                                auth()->user()->can(permissionConstant()::GERENCIAR_SECRETARIAS_LIST))
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button"
-                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                        Gerenciar
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        @can(permissionConstant()::GERENCIAR_USUARIOS_LIST)
-                                            <li><a class="dropdown-item" href="{{ route('get-users-list') }}">Usuários</a>
-                                            </li>
-                                        @endcan
-                                        @can(permissionConstant()::GERENCIAR_SECRETARIAS_LIST)
-                                            <li><a class="dropdown-item"
-                                                    href="{{ route('get-secretarias-list') }}">Secretarias</a></li>
-                                        @endcan
-                                    </ul>
                                 </li>
-                            @endif
-                            @if (auth()->user()->can(permissionConstant()::GERENCIAR_TIPOS_USUARIOS_LIST))
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button"
-                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                        Configurações
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        @can(permissionConstant()::GERENCIAR_TIPOS_USUARIOS_LIST)
-                                            <li>
-                                                <a class="dropdown-item" href="{{ route('get-roles-list') }}">Tipos de
-                                                    Usuários</a>
-                                            </li>
-                                        @endcan
-                                    </ul>
+                                @can(permissionConstant()::UNIDADE_SECRETARIA_LIST)
+                                <li><a class="dropdown-item" href="{{ route('unidades-secr-list') }}">Unidades</a></li>
+                                @endcan
+                            </ul>
+                        </li>
+                        @if (auth()->user()->can(permissionConstant()::GERENCIAR_USUARIOS_LIST) ||
+                        auth()->user()->can(permissionConstant()::GERENCIAR_SECRETARIAS_LIST))
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Gerenciar
+                            </a>
+                            <ul class="dropdown-menu">
+                                @can(permissionConstant()::GERENCIAR_USUARIOS_LIST)
+                                <li><a class="dropdown-item" href="{{ route('get-users-list') }}">Usuários</a>
                                 </li>
-                            @endif
-                        </div>
+                                @endcan
+                                @can(permissionConstant()::GERENCIAR_SECRETARIAS_LIST)
+                                <li><a class="dropdown-item" href="{{ route('get-secretarias-list') }}">Secretarias</a>
+                                </li>
+                                @endcan
+                            </ul>
+                        </li>
+                        @endif
+                        @if (auth()->user()->can(permissionConstant()::GERENCIAR_TIPOS_USUARIOS_LIST))
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Configurações
+                            </a>
+                            <ul class="dropdown-menu">
+                                @can(permissionConstant()::GERENCIAR_TIPOS_USUARIOS_LIST)
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('get-roles-list') }}">Tipos de
+                                        Usuários</a>
+                                </li>
+                                @endcan
+                            </ul>
+                        </li>
+                        @endif
                     </div>
                 </div>
+            </div>
             @endauth
     </header>
 
     <div class="container mt-3">
         @if (session('warning'))
-            <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <li>{{ session('warning') }}</li>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <li>{{ session('warning') }}</li>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
         @endif
         @if (session('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <li>{{ session('success') }}</li>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <li>{{ session('success') }}</li>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
         @endif
         @if (isset($errors) && $errors->any())
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
         @endif
         @yield('main')
     </div>
