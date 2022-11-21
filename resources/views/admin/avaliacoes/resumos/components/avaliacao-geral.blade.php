e797899d59fa2eaace2599eb9e8eb1b5f7bd72a9
SauloDc <sdc2decastro@gmail.com>
2022-11-21 14:36:03 -0300

    OUV-42 - inserindo validaçoes para quando não houver avaliações

--- resources/views/admin/avaliacoes/resumos/components/avaliacao-geral.blade.php
+++ resources/views/admin/avaliacoes/resumos/components/avaliacao-geral.blade.php
@@ -3,42 +3,41 @@
         <div class="card-header">
             <h4>{{$title}}</h4>
         </div>
-        <div class="">
-            <div class=" p-3">
-                <div class="d-flex justify-content-between align-items-end">
-                    <div class="text-center text-danger">
-                        <i class="@if ($avaliacoesAverage < 2) fa-6x @else fa-4x @endif  fa-regular fa-face-angry"></i>
-                        <br>
-                        <span>Muito Ruim</span>
-                    </div>
-                    <div class="text-center text-warning">
-                        <i
-                            class="@if ($avaliacoesAverage >= 2 && $avaliacoesAverage < 4) fa-6x @else fa-4x @endif fa-regular fa-face-frown"></i>
-                        <br>
-                        <span>Ruim</span>
-                    </div>
-                    <div class="text-center text-info">
-                        <i
-                            class="@if ($avaliacoesAverage >= 4 && $avaliacoesAverage < 6) fa-6x @else fa-4x @endif  fa-regular fa-face-meh"></i>
-                        <br>
-                        <span>Neutro</span>
-                    </div>
-
-                    <div class="text-center text-primary">
-                        <i
-                            class="@if ($avaliacoesAverage >= 6 && $avaliacoesAverage < 8) fa-6x @else fa-4x @endif  fa-regular fa-face-smile"></i>
-                        <br>
-                        <span>Bom</span>
-                    </div>
-                    <div class="text-center text-success">
-                        <i
-                            class="@if ($avaliacoesAverage >= 8) fa-6x @else fa-4x @endif fa-regular fa-face-laugh-beam"></i>
-                        <br>
-                        <span>Muito Bom</span>
-                    </div>
+        @if ($qtdAvaliacoes > 0)
+        <div class=" p-3">
+            <div class="d-flex justify-content-between align-items-end">
+                <div class="text-center text-danger">
+                    <i class="@if ($avaliacoesAverage < 2) fa-6x @else fa-4x @endif  fa-regular fa-face-angry"></i>
+                    <br>
+                    <span>Muito Ruim</span>
                 </div>
-                <div class="mt-2 progress">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated
+                <div class="text-center text-warning">
+                    <i
+                        class="@if ($avaliacoesAverage >= 2 && $avaliacoesAverage < 4) fa-6x @else fa-4x @endif fa-regular fa-face-frown"></i>
+                    <br>
+                    <span>Ruim</span>
+                </div>
+                <div class="text-center text-info">
+                    <i
+                        class="@if ($avaliacoesAverage >= 4 && $avaliacoesAverage < 6) fa-6x @else fa-4x @endif  fa-regular fa-face-meh"></i>
+                    <br>
+                    <span>Neutro</span>
+                </div>
+
+                <div class="text-center text-primary">
+                    <i
+                        class="@if ($avaliacoesAverage >= 6 && $avaliacoesAverage < 8) fa-6x @else fa-4x @endif  fa-regular fa-face-smile"></i>
+                    <br>
+                    <span>Bom</span>
+                </div>
+                <div class="text-center text-success">
+                    <i class="@if ($avaliacoesAverage >= 8) fa-6x @else fa-4x @endif fa-regular fa-face-laugh-beam"></i>
+                    <br>
+                    <span>Muito Bom</span>
+                </div>
+            </div>
+            <div class="mt-2 progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated
                                     @if ($avaliacoesAverage < 2)
                                         bg-danger
                                     @else
@@ -57,15 +56,15 @@
                                         @endif    
                                     @endif
                                     " role="progressbar" aria-label="Animated striped example"
-                        aria-valuenow="{{$percentAverage}}" aria-valuemin="0" aria-valuemax="100"
-                        style="width: {{$percentAverage}}%">
-                        {{number_format($percentAverage, 2,',', '')}} %
-                    </div>
+                    aria-valuenow="{{$percentAverage}}" aria-valuemin="0" aria-valuemax="100"
+                    style="width: {{$percentAverage}}%">
+                    {{number_format($percentAverage, 2,',', '')}} %
                 </div>
             </div>
-            <div class="text-center">
-                <span class="fs-3 fw-normal">Avaliação Geral:</span>
-                <p class="fs-1 fw-bold 
+        </div>
+        <div class="text-center">
+            <span class="fs-3 fw-normal">Avaliação Geral:</span>
+            <p class="fs-1 fw-bold 
                                     @if ($avaliacoesAverage < 2)
                                     text-danger
                                     @else
@@ -83,9 +82,15 @@
                                             @endif    
                                         @endif    
                                     @endif">
-                    {{ number_format($avaliacoesAverage, 2, ',', '') }}<span class="fs-3 fw-normal text-body">/10</span>
-                </p>
-            </div>
+                {{ number_format($avaliacoesAverage, 2, ',', '') }}<span class="fs-3 fw-normal text-body">/10</span>
+            </p>
         </div>
+        @else
+        <div class="m-3 alert alert-info">
+            <ul>
+                <li>Não existem avaliações </li>
+            </ul>
+        </div>
+        @endif
     </div>
 </div>
