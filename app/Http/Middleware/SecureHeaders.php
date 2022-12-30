c5204394bb17c288cc3227cc76b3dc410eef561a
SauloDc <sdc2decastro@gmail.com>
2022-12-30 09:31:39 -0300

    OUV-86 - Inserindo header de segurança para o controle da Cache

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -28,6 +28,7 @@ class SecureHeaders
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('Strict-Transport-Security', 'max-age:31536000; includeSubDomains');
+        $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
 
         return $response;
     }
