a949a1c5931236e7ba56b3c1bdee95364e54bc97
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-16 15:16:02 -0300

    Adicionando a funcionalidade de DarkMode nas páginas públicas do sistema

--- resources/views/public/politicas.blade.php
+++ resources/views/public/politicas.blade.php
@@ -10,14 +10,16 @@
 
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}" nonce="{{ csp_nonce() }}">
 </head>
-<body>
+<body class="dark:bg-black">
       <!-- Inicio Navbar -->
-      <nav class="navbar bg-second-color">
-        <div class="navbar-content bg-second-color mx-3">
+      <nav class="navbar bg-second-color dark:bg-black dark:border">
+        <div class="navbar-content bg-second-color mx-3 dark:bg-black">
             <img src="{{ asset('imgs/logo/logo.png') }}" nonce="{{ csp_nonce() }}" alt="Celke" class="logo">
         </div>
         
         <div class="navbar-content mx-3">
+            <img src="{{ asset('imgs/icon/moon.svg')}}" class="moon cursor-pointer mx-2" alt="moon" width="15px">
+            <img src="{{ asset('imgs/icon/sun.svg')}}" class="sun cursor-pointer mx-2" alt="sun" witdh="15px">
             <a href="{{route('login')}}" class="nav-link text-white mx-2">Login</a>
             <a href="{{route('register')}}" class="nav-link bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium">Cadastrar</a>
         </div>
