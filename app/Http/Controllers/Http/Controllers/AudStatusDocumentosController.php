f734fa0a26aebb81391d31d613236e7f41105cdd
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-19 16:47:49 -0300

    Criando controller referente a tabela de aud_status_documentos

---
+++ app/Http/Controllers/Http/Controllers/AudStatusDocumentosController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AudStatusDocumentosController extends Controller
+{
+    //
+}
