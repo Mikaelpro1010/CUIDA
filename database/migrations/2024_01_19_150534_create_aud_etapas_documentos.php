3ac505d384ab28bf83012315253dc74ea89341cb
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-19 15:10:39 -0300

    criando a migration referente a tabela de aud_etapas_documentos

---
+++ database/migrations/2024_01_19_150534_create_aud_etapas_documentos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAudEtapasDocumentos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aud_etapas_documentos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('icone');
+            $table->string('lado_timeline');
+            $table->integer('cadastrado_por');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
