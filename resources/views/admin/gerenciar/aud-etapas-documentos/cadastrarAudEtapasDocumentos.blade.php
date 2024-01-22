75054ce0e4ba05c75019143ba9cb609d51121a55
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-22 15:32:30 -0300

    Adicionando a página de cadastro referente a tabela de aud_etapas_documentos

---
+++ resources/views/admin/gerenciar/aud-etapas-documentos/cadastrarAudEtapasDocumentos.blade.php
@@ -0,0 +1,44 @@
+@extends('template.base')
+
+@section('content')
+<!-- Inicio do conteudo do administrativo -->
+<div class="row p-3">
+    <div class="top-list">
+        @include('componentes/flash-message')
+        <span class="title-content">Formulário</span>
+        <div class="top-list-right">
+            <a href="{{ route('listarAudEtapasDocumentos') }}" class="btn btn-outline-info">Listar</a>
+        </div>
+    </div>
+    
+    <div class="content-adm">
+        <form class="form-adm" action="{{route('cadastrarAudEtapasDocumentos')}}" method="post">
+            {{ csrf_field() }}
+            <div class="row-input">
+                <div class="column">
+                    <label class="title-input">Nome</label>
+                    <input type="text" name="nome" id="nome" class="input-adm" placeholder="Nome completo">
+                </div>
+                <div class="column">
+                    <label class="title-input">Icone</label>
+                    <input type="text" name="icone" id="icone" class="input-adm">
+                </div>
+                <div class="column">
+                    <label class="title-input">Lado Timeline</label>
+                    <input type="text" name="lado_timeline" id="lado_timeline" class="input-adm">
+                </div>
+                <div class="column">
+                    <label class="title-input">Cadastrado Por</label>
+                    <input type="number" name="cadastrado_por" id="cadastrado_por" class="input-adm">
+                </div>
+            </div>
+    
+            <div class="text-center">
+                <button type="submit" class="btn btn-outline-success">Cadastrar</button>
+            </div>
+        </form>
+    </div>
+    @endcan
+    <!-- Fim do conteudo do administrativo -->
+</div>
+@endsection
