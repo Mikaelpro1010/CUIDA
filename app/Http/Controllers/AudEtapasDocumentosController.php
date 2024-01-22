6f960ed5c9af57d7bbd9b4e1ebae79be1328ce5d
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-22 14:07:19 -0300

    criando a funcionalidade de listagem referente a tabela de aud_etapas_documentos

--- app/Http/Controllers/AudEtapasDocumentosController.php
+++ app/Http/Controllers/AudEtapasDocumentosController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Constants\Permission;
 use Illuminate\Http\Request;
+use App\Models\AudEtapasDocumentos;
 
 class AudEtapasDocumentosController extends Controller
 {
-    //
+    public function listarAudEtapasDocumentos(){
+        $AudEtapasDocumentos = AudEtapasDocumentos::query()
+        ->paginate(10);
+        return view('admin/gerenciar/aud-etapas-documentos/listarAudEtapasDocumentos', compact('AudEtapasDocumentos'));
+    }
 }
