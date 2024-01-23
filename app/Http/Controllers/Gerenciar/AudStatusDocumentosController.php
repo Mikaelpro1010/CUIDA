4151b59300d106f1c7d43e90f7b19d3170ae2c7d
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-23 15:38:51 -0300

    Criando a funcionalidade de listagem referente a tabela de aud_prazos_documentos

--- app/Http/Controllers/Gerenciar/AudStatusDocumentosController.php
+++ app/Http/Controllers/Gerenciar/AudStatusDocumentosController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Gerenciar;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Constants\Permission;
+use Illuminate\Http\Request;
+use App\Models\AudStatusDocumentos;
 
 class AudStatusDocumentosController extends Controller
 {
-    //
+    public function listarAudStatusDocumentos(){
+        $AudStatusDocumentos = AudStatusDocumentos::query()
+        ->paginate(10);
+        return view('admin/gerenciar/aud-status-documentos/listarAudStatusDocumentos', compact('AudStatusDocumentos'));
+    }
 }
