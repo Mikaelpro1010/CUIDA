ddfb642318310c058f22bb21e5a973056dc9728f
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-23 13:34:20 -0300

    Realizando ajustes de rotas na pagina de listagem

--- resources/views/admin/gerenciar/aud-etapas-documentos/listarAudEtapasDocumentos.blade.php
+++ resources/views/admin/gerenciar/aud-etapas-documentos/listarAudEtapasDocumentos.blade.php
@@ -6,13 +6,11 @@
 <div class="row p-3">
     <div class="top-list">
         @include('componentes/flash-message')
-        
-            <span class="title-content">Listar</span>
-            <div class="top-list-right">
-                <a href="{{route('visualizarAudEtapaDocumento')}}" class="btn btn-outline-success">Cadastrar</a>
-                <!--<button type="button" class="btn-success"><i class="fa-solid fa-square-plus"></i></button>-->
-            </div>
-        @endcan
+        <span class="title-content">Listar</span>
+        <div class="top-list-right">
+            <a href="{{route('visualizarCadastroAudEtapasDocumentos')}}" class="btn btn-outline-success">Cadastrar</a>
+            <!--<button type="button" class="btn-success"><i class="fa-solid fa-square-plus"></i></button>-->
+        </div>
     </div>
     
     <div class="table-responsive">
@@ -36,8 +34,8 @@
                     <td class="list-body-content">{{$AudEtapaDocumento->lado_timeline}}</td>
                     <td class="list-body-content table-sm-none">{{$AudEtapaDocumento->cadastrado_por}}</td>
                     <td class="list-body-content">
-                        <a class="btn btn-outline-primary" href="{{ route('visualizarAudEtapasDocumentos', $AudEtapaDocumento) }}">Visualizar</a>
-                        <a class="btn btn-outline-warning" href="{{ route('editarAudEtapasDocumentos', $AudEtapaDocumento) }}">Editar</a>
+                        <a class="btn btn-outline-primary" href="{{ route('visualizarAudEtapaDocumento', $AudEtapaDocumento) }}">Visualizar</a>
+                        <a class="btn btn-outline-warning" href="{{ route('editarAudEtapaDocumento', $AudEtapaDocumento) }}">Editar</a>
                         <a class="btn btn-outline-danger" href="javascript:deleteItem({{ $AudEtapaDocumento->id }})">Deletar</a>
                     </td>
                 </tr>
@@ -46,7 +44,6 @@
         </table>
     </div>
     
-    @endcan
     
     <div class="justify-content-evenly">
         {{ $AudEtapasDocumentos->links('pagination::bootstrap-4') }}
@@ -60,7 +57,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form class="" action="{{ route('deletarAluno') }}" method="POST">
+                    <form class="" action="{{ route('deletarAudEtapaDocumento') }}" method="POST">
                         <p>Tem certeza que deseja excluir esses dados?</p>
                         {{ csrf_field() }}
                         <input type="hidden" id="deletar" name="id" value="">
@@ -73,10 +70,9 @@
             </div>
         </div>
     </div>
-    @endcan
     <!-- Fim do conteudo do administrativo -->
-    @endsection
 </div>
+@endsection
 
 
 @push('scripts')
