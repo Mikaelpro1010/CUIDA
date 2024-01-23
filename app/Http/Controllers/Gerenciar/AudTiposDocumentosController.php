8ae8bbbeafc225003c46cdb47f31cccc4086523d
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-23 16:31:03 -0300

    Criando a funcionalidade de listagem referente a tabela de aud_tipos_documentos

--- app/Http/Controllers/Gerenciar/AudTiposDocumentosController.php
+++ app/Http/Controllers/Gerenciar/AudTiposDocumentosController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Gerenciar;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Constants\Permission;
+use Illuminate\Http\Request;
+use App\Models\AudTiposDocumentos;
 
 class AudTiposDocumentosController extends Controller
 {
-    //
+    public function listarAudTiposDocumentos(){
+        $AudTiposDocumentos = AudTiposDocumentos::query()
+        ->paginate(10);
+        return view('admin/gerenciar/aud-tipos-documentos/listarAudTiposDocumentos', compact('AudTiposDocumentos'));
+    }
 }
