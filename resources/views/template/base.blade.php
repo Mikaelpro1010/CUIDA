ad7d7b9d8fb71d9d23581640dc6aafe0955db3d5
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-26 16:45:04 -0300

    Implementando mensagens de erro referente aos campos de formulário do sistema

--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -4,6 +4,16 @@
     <!-- Inicio do conteudo do administrativo -->
     <main class="wrapper dark:bg-black dark:border">
         <div class="card">
+            <!-- Exibição de mensagens de erro -->
+            @if($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
             @yield('content')
         </div>
     </main>
