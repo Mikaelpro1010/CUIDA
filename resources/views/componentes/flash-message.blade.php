04395c07cf96c48f3d749c647ba113e7daa6123a
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-01-29 13:38:54 -0300

    Realizando ajustes de layout nas mensagens de erro do sistema

--- resources/views/componentes/flash-message.blade.php
+++ resources/views/componentes/flash-message.blade.php
@@ -1,12 +1,12 @@
 @if(session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <div class="alert alert-success alert-dismissible fade show p-3" role="alert">
         <li>{{ session('success') }}</li>
         <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
 @if(session('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <div class="alert alert-danger alert-dismissible fade show p-3" role="alert">
         <li>{{ session('error') }}</li>
         <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
     </div>
--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -3,15 +3,13 @@
 @section('main')
     <!-- Inicio do conteudo do administrativo -->
     <main class="wrapper dark:bg-black dark:border">
-        <div class="card">
+        <div class="card p-3">
             <!-- Exibição de mensagens de erro -->
             @if($errors->any())
             <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
             </div>
             @endif
             @yield('content')
