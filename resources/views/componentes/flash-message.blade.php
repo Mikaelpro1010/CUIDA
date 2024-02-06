d1ca7fed302c9e53be5abe890b17f19902ff3975
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-02-06 15:55:50 -0300

    Adicionando cor de fundo nas mensagens de alert

--- resources/views/componentes/flash-message.blade.php
+++ resources/views/componentes/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if(session('success'))
-    <div class="alert alert-success alert-dismissible fade show p-3" role="alert">
+    <div class="alert alert-success alert-dismissible bg-success text-white fade show p-3" role="alert">
         <span>{{ session('success') }}</span>
         {{-- <li>{{ session('success') }}</li> --}}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -7,7 +7,7 @@
 @endif
 
 @if(session('error'))
-    <div class="alert alert-danger alert-dismissible fade show p-3" role="alert">
+    <div class="alert alert-danger alert-dismissible bg-danger text-white fade show p-3" role="alert">
         <span>{{ session('error') }}</span>
         {{-- <li>{{ session('error') }}</li> --}}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
