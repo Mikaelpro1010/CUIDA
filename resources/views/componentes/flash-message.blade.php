c3a700181d03dcea0b97b991a47bf5cb456955cd
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-02-07 16:36:49 -0300

    Realizando alterações no layout das mensagens de erro

--- resources/views/componentes/flash-message.blade.php
+++ resources/views/componentes/flash-message.blade.php
@@ -1,7 +1,6 @@
 @if(session('success'))
     <div class="alert alert-success alert-dismissible bg-success text-white fade show p-3" role="alert">
         <span>{{ session('success') }}</span>
-        {{-- <li>{{ session('success') }}</li> --}}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
@@ -9,7 +8,6 @@
 @if(session('error'))
     <div class="alert alert-danger alert-dismissible bg-danger text-white fade show p-3" role="alert">
         <span>{{ session('error') }}</span>
-        {{-- <li>{{ session('error') }}</li> --}}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
