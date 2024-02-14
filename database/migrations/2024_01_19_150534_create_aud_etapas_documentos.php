6e1ee349a83be1c7715f6781fc785b4fbb6954ff
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-02-14 16:51:39 -0300

    Adicionando coluna de softDeletes na tabela de aud_etapas_documentos

--- database/migrations/2024_01_19_150534_create_aud_etapas_documentos.php
+++ database/migrations/2024_01_19_150534_create_aud_etapas_documentos.php
@@ -21,6 +21,7 @@ class CreateAudEtapasDocumentos extends Migration
             $table->unsignedBigInteger('usuario_id');
             $table->foreign('usuario_id')->references('id')->on('users');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
