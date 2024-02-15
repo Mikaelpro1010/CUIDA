77a9fe8244eb37e0df6f2823f24a1119d01fac3e
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-02-15 14:12:11 -0300

    Implementando o método de SoftDeletes no model da tabela de aud_etapas_documentos

--- app/Models/AudEtapasDocumentos.php
+++ app/Models/AudEtapasDocumentos.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AudEtapasDocumentos extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = [
         'nome', 'icone', 'lado_timeline', 'usuario_id',
     ];
